2baadfbc3964cd119b5cc18c6fc9bcd12541070c
m3d3l1n3 <82612183+m3d3l1n3@users.noreply.github.com>
2022-09-23 19:09:57 +0300

    Drawing and  writing on an image C#
    This solution provides code for drawing and writing on an image(pictureBox), on a WindowForms element.

---
+++ DrawingBits/DrawingBits/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace DrawingBits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbPicture = new System.Windows.Forms.PictureBox();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbPicture
+            // 
+            this.pbPicture.BackgroundImage = global::DrawingBits.Properties.Resources.WhatsApp_Image_2022_02_16_at_15_03_30;
+            this.pbPicture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbPicture.InitialImage = null;
+            this.pbPicture.Location = new System.Drawing.Point(394, 44);
+            this.pbPicture.Name = "pbPicture";
+            this.pbPicture.Size = new System.Drawing.Size(372, 432);
+            this.pbPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPicture.TabIndex = 0;
+            this.pbPicture.TabStop = false;
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(38, 100);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(106, 20);
+            this.tbX.TabIndex = 1;
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(38, 163);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(106, 20);
+            this.tbY.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "y";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(210, 252);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 5;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Value";
+            // 
+            // tbValue
+            // 
+            this.tbValue.Location = new System.Drawing.Point(38, 225);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(100, 20);
+            this.tbValue.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 509);
+            this.Controls.Add(this.tbValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.pbPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPicture;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbValue;
+    }
+}
+
---
+++ DrawingBits/DrawingBits/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace DrawingBits
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Render();
+        }
+        void Render()
+        {
+            
+            using (var bmp = (Bitmap)pbPicture.Image?.Clone() ?? new Bitmap(pbPicture.Width, pbPicture.Height))
+            using (var gfx = Graphics.FromImage(bmp))
+            using (var pen = new Pen(Color.DeepPink))
+            {
+                // draw/write the value from textbox tbValue
+                string firstText = tbValue.Text;
+                PointF firstLocation = new PointF(Convert.ToInt32(tbX.Text), Convert.ToInt32(tbY.Text));
+                using (Graphics graphics = Graphics.FromImage(bmp))
+                {
+                    using (Font arialFont = new Font("Arial", 12))
+                    {
+                        graphics.DrawString(firstText, arialFont, Brushes.DeepPink, firstLocation);
+                    }
+
+                }
+
+                System.Drawing.Rectangle rect = new System.Drawing.Rectangle();
+                rect.X = Convert.ToInt32(tbX.Text);
+                rect.Y = Convert.ToInt32(tbY.Text);
+                rect.Width = 20;
+                rect.Height = 20;
+                gfx.DrawEllipse(pen, rect);
+               
+                // copy the bitmap to the picturebox
+                pbPicture.Image?.Dispose();
+                pbPicture.Image = (Bitmap)bmp.Clone();
+            }
+        }
+
+    }
+}
+
+//string firstText = "Hello";
+//string secondText = "World";
+
+//PointF firstLocation = new PointF(10f, 10f);
+//PointF secondLocation = new PointF(10f, 50f);
+
+//string imageFilePath = @"path\picture.bmp"
+//Bitmap bitmap = (Bitmap)Image.FromFile(imageFilePath);//load the image file
+
+//using (Graphics graphics = Graphics.FromImage(bitmap))
+//{
+//    using (Font arialFont = new Font("Arial", 10))
+//    {
+//        graphics.DrawString(firstText, arialFont, Brushes.Blue, firstLocation);
+//        graphics.DrawString(secondText, arialFont, Brushes.Red, secondLocation);
+//    }
+//}
+
+//bitmap.Save(imageFilePath);//save the image file
